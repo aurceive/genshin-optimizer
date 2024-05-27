75db47da45da850370cde60753ef9eb14dee97f9
Estis <kulaykin2000@gmail.com>
2024-05-27 19:15:53 +0500

    Updates

---
+++ assets/Upload-B7ukNt8a.js
@@ -0,0 +1,1 @@
+import{s as a,t as r,v as t}from"./index-3j9gmnXW.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var v=e.default=void 0,o=u(a()),d=r(),l=(0,o.default)((0,d.jsx)("path",{d:"M5 20h14v-2H5v2zm0-10h4v6h6v-6h4l-7-7-7 7z"}),"Upload");v=e.default=l;export{v as d};
