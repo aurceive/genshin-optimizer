012ff30c0a40c1db8d02e67a262b1811f42901e1
Estis <kulaykin2000@gmail.com>
2024-06-11 09:09:20 +0500

    Updates

---
+++ assets/useTeamChar-CXKYc07t.js
@@ -0,0 +1,1 @@
+import{a as s,fC as t}from"./index-Cx8TvA4z.js";function u(a){const e=s();return t(e.teamChars,a)}export{u};
--- beta/frontend/assets/useTeamChar-mVL5z-8S.js
+++
@@ -1,1 +0,0 @@
-import{a as s,fC as t}from"./index-D9uHclzX.js";function u(a){const e=s();return t(e.teamChars,a)}export{u};
